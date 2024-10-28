eeb91f347151b39eb9b97200d14fa0153455c170
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-10-28 19:22:05 +0100

    Add a new component - a draggable list

--- Responsive_slider/js/responsiveSlider.js
+++ Responsive_slider/js/responsiveSlider.js
@@ -1,4 +1,4 @@
-let carousel;
+let carouselList;
 let firstCard;
 let arrowButtons;
 let allCarouselCards;
@@ -14,57 +14,57 @@ function main() {
 };
 
 function getElements() {
-  carousel = document.querySelector(".wrapper__list");
+  carouselList = document.querySelector(".wrapper__list");
   firstCard = document.querySelector(".wrapper__carrousel-card");
   arrowButtons = document.querySelectorAll(".wrapper__button");
-  allCarouselCards = [...carousel.firstElementChild.children];
+  allCarouselCards = [...carouselList.firstElementChild.children];
 
   cardWidth = firstCard.offsetWidth + 16;
 };
 
 function addListeners() {
-  carousel.addEventListener("mousemove", dragSlider);
-  carousel.addEventListener("mousedown", startDragging);
+  carouselList.addEventListener("mousemove", dragSlider);
+  carouselList.addEventListener("mousedown", startDragging);
   arrowButtons.forEach((button) => button.addEventListener("click", handleArrowButton));
   document.addEventListener("mouseup", stopDragging);
 }
 
 const startDragging = (event) => {
   isDragging = true;
-  carousel.classList.add("wrapper__list--dragging");
-  // Record the initial cursor and scroll position of the carousel
+  carouselList.classList.add("wrapper__list--dragging");
+  // Record the initial cursor and scroll position of the carouselList
   cursorPosition = event.pageX; // Ustawiam pozycję kursora, w której przewijanie się rozpoczęło
-  startScrollLeft = carousel.scrollLeft; // Ustawiam wstępną pozycję przewijania
+  startScrollLeft = carouselList.scrollLeft; // Ustawiam wstępną pozycję przewijania
 }
 
 const dragSlider = (event) => {
   if (!isDragging) return;
-  // Update the scroll position of the carousel based on the cursor movement
-  carousel.scrollLeft = startScrollLeft - (event.pageX - cursorPosition);
+  // Update the scroll position of the carouselList based on the cursor movement
+  carouselList.scrollLeft = startScrollLeft - (event.pageX - cursorPosition);
 }
 
 const stopDragging = () => {
   isDragging = false;
-  carousel.classList.remove("wrapper__list--dragging");
+  carouselList.classList.remove("wrapper__list--dragging");
 }
 
 const handleArrowButton = (event) => {
   const arrowButton = event.currentTarget;
 
-  carousel.scrollLeft = arrowButton.classList[1] === "wrapper__button--left"
-    ? carousel.scrollLeft - cardWidth
-    : carousel.scrollLeft + cardWidth;
+  carouselList.scrollLeft = arrowButton.classList[1] === "wrapper__button--left"
+    ? carouselList.scrollLeft - cardWidth
+    : carouselList.scrollLeft + cardWidth;
 }
 
 const handleAutoplay = () => {
-  let cardsPerView = Math.round(carousel.offsetWidth / cardWidth);
+  let cardsPerView = Math.round(carouselList.offsetWidth / cardWidth);
   
   allCarouselCards.slice(0, -cardsPerView).reverse().forEach((card) => {
-    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+    carouselList.insertAdjacentHTML("afterbegin", card.outerHTML);
   });
 
   allCarouselCards.slice(0, cardsPerView).reverse().forEach((card) => {
-    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+    carouselList.insertAdjacentHTML("beforeend", card.outerHTML);
   });
 }
 
